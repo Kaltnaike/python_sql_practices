c5c6a30371f9e5f6dfe499f080e074a28261ef5c
Kaltnaike <purpleduralumin@gmail.com>
2022-07-06 14:53:18 +0100

    Master

---
+++ python_basics/hello_world.py
@@ -0,0 +1,36 @@
+#This is a python hello world program
+#print("Hello World!")
+
+#Create a multi-line string
+#multi_line= """
+#This is a multi line string
+#"""
+#print(multi_line)
+
+#declare variable
+# greeting="Hello T-baby Sexy"
+# print(greeting)
+
+#data types
+from operator import truediv
+
+
+x=23
+y=20.5
+z="tawa"
+a=1j
+b=None
+c=["love","pain","death"]
+d={"love","pain","death"}
+e=("love","pain","death")
+f=True
+
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
